42ac62cb8d2dee064d90dd83d86a77c4376fc4c6
Hasmik23 <ht.ohannesian@gmail.com>
2023-06-01 19:59:25 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+
+                       for (int i = 0; i < 10; i++) {
+                    System.out.println("Hello world!");
+                    // Thread.sleep(2000);  // change to
+                    Thread.sleep(2000);   //then commit and push
+
+                }
+            }
+       }
+
+

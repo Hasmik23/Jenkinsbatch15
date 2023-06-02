fc2c8e38847acf97ba5bec5b77b0e1391d800731
Hasmik23 <ht.ohannesian@gmail.com>
2023-06-01 21:16:48 -0400

    Minor changes

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) throws InterruptedException {
 
-                       for (int i = 0; i < 20; i++) {  //change 1<10
+                       for (int i = 0; i < 25; i++) {  //change 1<10
                     System.out.println("Hello world!");
                     // Thread.sleep(2000);  // change to
                     Thread.sleep(1000);   //then commit and push
